5d00bd18c5eeb29d5f130e833df60568f865976a
Ankit <ankitrjshrivastav@gmail.com>
2023-05-18 20:01:02 +0530

    server url fixed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://baat-chit-api.onrender.com",
+  baseURL: "https://baat-chit-api.onrender.com/api",
 });
 
 export const getRoomExists = (roomId) => API.get(`/room-exists/${roomId}`);
